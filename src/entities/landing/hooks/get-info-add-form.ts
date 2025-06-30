8d370ea56aadf1d06b4f9ecdf4ee1d5e8901abd6
Alisher <alik20021223@gmail.com>
2025-06-30 20:05:14 +0500

    did hook

--- src/entities/landing/hooks/get-info-add-form.ts
+++ src/entities/landing/hooks/get-info-add-form.ts
@@ -1,23 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
+import {
+  useQuery,
+  UseQueryOptions,
+  UseQueryResult,
+} from "@tanstack/react-query";
 import landingsService from "@/entities/landing/service/index.service";
 import { InfoAddFormResponseType } from "@entities/landing/types";
+import { MockInitialData } from "../mock";
 
-const initialData: InfoAddFormResponseType = {
-  user: {
-    id: 1,
-    role: "",
-  },
-  channels: [],
-  domains: [],
-};
+const initialData: InfoAddFormResponseType = MockInitialData;
 
-export function useQueryInfoAddForm() {
-  const { isLoading, isError, data, error, refetch } = useQuery({
+export function useQueryInfoAddForm(
+  options?: Partial<UseQueryOptions<InfoAddFormResponseType, Error>>
+): UseQueryResult<InfoAddFormResponseType, Error> {
+  return useQuery<InfoAddFormResponseType, Error>({
     queryKey: ["info-add-form"],
     queryFn: () => landingsService.getInfoAddForm(),
     retry: false,
-    initialData: initialData,
+    initialData,
+    ...options,
   });
-
-  return { isLoading, isError, data: data, error, refetch };
 }
