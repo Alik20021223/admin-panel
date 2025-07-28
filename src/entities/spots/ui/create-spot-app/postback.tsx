f70adf58ec095b61d098e43d0a0bd26310ab5ad3
Alisher <alik20021223@gmail.com>
2025-07-28 14:47:09 +0500

    did fix

--- src/entities/spots/ui/create-spot-app/postback.tsx
+++ src/entities/spots/ui/create-spot-app/postback.tsx
@@ -1,41 +1,41 @@
-import AddPixel from "@feature/add-pixel";
-import { useForm } from "react-hook-form";
-import { Form } from "@shadcdn/form";
-import { Plus } from "lucide-react";
-import { Button } from "@shadcdn/button";
-import { AppSpotPostBackType } from "@entities/spots/types";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { appSpotPostBackSchema } from "./validation";
+// import AddPixel from "@feature/add-pixel";
+// import { useForm } from "react-hook-form";
+// import { Form } from "@shadcdn/form";
+// import { Plus } from "lucide-react";
+// import { Button } from "@shadcdn/button";
+// import { AppSpotPostBackType } from "@entities/spots/types";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { appSpotPostBackSchema } from "./validation";
 
 
-const PostBackTab = () => {
+// const PostBackTab = () => {
 
-    const form = useForm<AppSpotPostBackType>({
-        mode: "onChange",
-        resolver: zodResolver(appSpotPostBackSchema),
-        defaultValues: {
-            postBack: [],
-        }
-    });
+//     const form = useForm<AppSpotPostBackType>({
+//         mode: "onChange",
+//         resolver: zodResolver(appSpotPostBackSchema),
+//         defaultValues: {
+//             postBack: [],
+//         }
+//     });
 
-    const onSubmitForm = (data: AppSpotPostBackType) => {
-        console.log(data);
-    }
+//     const onSubmitForm = (data: AppSpotPostBackType) => {
+//         console.log(data);
+//     }
 
-    return (
-        <>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmitForm)}>
-                    <AddPixel name="postBack" />
+//     return (
+//         <>
+//             <Form {...form}>
+//                 <form onSubmit={form.handleSubmit(onSubmitForm)}>
+//                     <AddPixel name="postBack" />
 
-                    <div className="mt-5">
-                        <Button type="submit" className="space-x-2 w-full"><Plus className="w-4 h-4" />Создать спот</Button>
-                    </div>
-                </form>
-            </Form>
+//                     <div className="mt-5">
+//                         <Button type="submit" className="space-x-2 w-full"><Plus className="w-4 h-4" />Создать спот</Button>
+//                     </div>
+//                 </form>
+//             </Form>
 
-        </>
-    )
-}
+//         </>
+//     )
+// }
 
-export default PostBackTab
+// export default PostBackTab
